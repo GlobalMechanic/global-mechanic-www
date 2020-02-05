2473bb33b7f62b0cd62111dfa2b5e51b03d2f3c6
ben@globalmechanic.com <ben@globalmechanic.com>
2020-02-05 11:44:05 -0800

    created legacy routing

--- src/server/middleware/index.ts
+++ src/server/middleware/index.ts
@@ -22,6 +22,7 @@ export default function (this: WebsiteApplication): void {
 
     const PUBLIC_URL = app.get('public') as string
 
+    app.get('/assets/gm-2017-signature.png', legacySignatureRedirect(app))
     app.get('/file/:key', fileServe())
     app.get('/data', pageData)
     // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
---
+++ src/server/middleware/legacy-signature-redirect.ts
@@ -0,0 +1,34 @@
+import fs from 'fs'
+
+import { WebsiteApplication } from '../types'
+import { Request, Response } from 'express'
+
+/***************************************************************/
+// Modules State
+/***************************************************************/
+
+let legacySignaturePath: string
+
+function findLegacySiganturePath(app: WebsiteApplication): void {
+
+    const PUBLIC_URL = app.get('public') as string
+
+    const publicNames = fs.readdirSync(PUBLIC_URL)
+    for (const publicName of publicNames)
+        if (publicName.includes('signature'))
+            legacySignaturePath = '/' + publicName
+}
+
+/***************************************************************/
+// Exports
+/***************************************************************/
+
+// eslint-disable-next-line
+export default function (app: WebsiteApplication) {
+
+    findLegacySiganturePath(app)
+
+    return function (_req: Request, res: Response): void {
+        res.redirect(301, legacySignaturePath)
+    }
+}
