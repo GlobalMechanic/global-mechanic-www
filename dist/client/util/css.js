eb0d493d4e230085b2a8ed5e7d9f9c649dba79e3
ben@globalmechanic.com <ben@globalmechanic.com>
2020-02-05 13:22:33 -0800

    build artifacts updated

--- dist/client/util/css.js
+++ dist/client/util/css.js
@@ -5,32 +5,34 @@ const styled_components_1 = require("styled-components");
 // Exports
 /***************************************************************/
 exports.titleFont = styled_components_1.css `
-
     font-family: "Chronicle Cond A", "Chronicle Cond B";
     font-style: normal;
     font-weight: 700;
-
 `;
 exports.titleFontItalic = styled_components_1.css `
-
     font-family: "Chronicle Cond A", "Chronicle Cond B";
     font-style: italic;
     font-weight: 700;
-
 `;
 exports.bodyFont = styled_components_1.css `
-
-    font-family: "Ringside Condensed A", "Ringside Condensed B";
+    font-family: "Whitney Narrow A", "Whitney Narrow B";
     font-style: normal;
-    font-weight: 300;
-
+    font-weight: 400;
+`;
+exports.bodyFontItalic = styled_components_1.css `
+    font-family: "Whitney Narrow A", "Whitney Narrow B";
+    font-style: italic;
+    font-weight: 400;
 `;
 exports.bodyFontBold = styled_components_1.css `
-
-    font-family: "Ringside Condensed A", "Ringside Condensed B";
+    font-family: "Whitney Narrow A", "Whitney Narrow B";
     font-style: normal;
     font-weight: 700;
-
+`;
+exports.bodyFontBoldItalic = styled_components_1.css `
+    font-family: "Whitney Narrow A", "Whitney Narrow B";
+    font-style: italic;
+    font-weight: 700;
 `;
 exports.content = styled_components_1.css `
     box-sizing: border-box;
